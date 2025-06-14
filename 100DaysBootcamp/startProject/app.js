b8020a20bdfcb548dec666b830fa94a79c32a375
전재명 <jeonjm0130@naver.com>
2025-06-14 12:46:47 +0900

    51days

--- 100DaysBootcamp/startProject/app.js
+++ 100DaysBootcamp/startProject/app.js
@@ -1,35 +1,67 @@
 const express = require("express");
 
+const fs = require("fs");
+
 const path = require("path");
 
 const app = express();
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.use(express.static("public")); // 정적인 파일들을(.css ,image ,.js) 클라이언트에게 보내주는 전처리 미들웨어 보내주어야만 서버에서 해당 파일을 link가 가능하다.
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "index.html");
-  res.sendFile(htmlFilePath);
+  // const htmlFilePath = path.join(__dirname, "views", "index.html");
+  // res.sendFile(htmlFilePath);
+
+  res.render("index"); //템플릿 엔진을 사용할때 view파일을 호출하는 방법
 });
 
 app.get("/restaurants", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "restaurants.html");
-  res.sendFile(htmlFilePath); // sendFile - html 파일을 건낼 수 있는 함수
+  // const htmlFilePath = path.join(__dirname, "views", "restaurants.html");
+  // res.sendFile(htmlFilePath); // sendFile - html 파일을 건낼 수 있는 함수
+
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  res.render("restaurants", {
+    numberOfRestaurants: storedRestaurants.length,
+    restaurants: storedRestaurants,
+  });
 });
 
 app.get("/recommend", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "recommend.html");
-  res.sendFile(htmlFilePath); // sendFile - html 파일을 건낼 수 있는 함수
+  // const htmlFilePath = path.join(__dirname, "views", "recommend.html");
+  // res.sendFile(htmlFilePath); // sendFile - html 파일을 건낼 수 있는 함수
+  res.render("recommend");
+});
+
+app.post("/recommend", function (req, res) {
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  storedRestaurants.push(restaurant);
+
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+  res.redirect("/confirm");
 });
 
 app.get("/confirm", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "confirm.html");
-  res.sendFile(htmlFilePath); // sendFile - html 파일을 건낼 수 있는 함수
+  // const htmlFilePath = path.join(__dirname, "views", "confirm.html");
+  // res.sendFile(htmlFilePath); // sendFile - html 파일을 건낼 수 있는 함수
+  res.render("confirm");
 });
 
 app.get("/about", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "about.html");
-  res.sendFile(htmlFilePath); // sendFile - html 파일을 건낼 수 있는 함수
+  // const htmlFilePath = path.join(__dirname, "views", "about.html");
+  // res.sendFile(htmlFilePath); // sendFile - html 파일을 건낼 수 있는 함수
+  res.render("about");
 });
-
 
 app.listen(3000);
