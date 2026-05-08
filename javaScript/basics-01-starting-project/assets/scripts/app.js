a9d457782afb8f4cff68bba040f1ee6e295c9f8e
전재명 <jeonjm0130@naver.com>
2026-05-08 22:52:18 +0900

    js std 3

--- javaScript/basics-01-starting-project/assets/scripts/app.js
+++ javaScript/basics-01-starting-project/assets/scripts/app.js
@@ -6,12 +6,56 @@ function getUserInput(){
     return parseInt(userInput.value);
 }
 
+
+function createAndWriteOutput(연산자, 결과전값, 투입값){
+    const calcDescription = `${결과전값} ${연산자} ${투입값}`;
+    outputResult(currentResult, calcDescription);
+}
+
 function add(){
     const enterNumber = getUserInput();
-    const calcDescription = `${currentResult} + ${enterNumber}`;
+    const initialResult = currentResult;
     currentResult = currentResult + enterNumber;
-    outputResult(currentResult, calcDescription);
+    createAndWriteOutput('+', initialResult, enterNumber);
+    
      
 }
 
+function subtract(){
+    const enterNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enterNumber;
+    createAndWriteOutput('-', initialResult, enterNumber);
+     
+}
+
+function multiply(){
+    const enterNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enterNumber;
+    createAndWriteOutput('*', initialResult, enterNumber);
+     
+}
+
+function divide(){
+    const enterNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enterNumber;
+    createAndWriteOutput('/', initialResult, enterNumber);
+     
+}
+
+function initalizeResult(){
+    currentResult = 0;
+    outputResult(currentResult, "back to Zero");
+}
+
 addBtn.addEventListener('click', add);
+
+subtractBtn.addEventListener('click', subtract);
+
+multiplyBtn.addEventListener('click', multiply);
+
+divideBtn.addEventListener('click', divide);
+
+initBtn.addEventListener('click', initalizeResult);
