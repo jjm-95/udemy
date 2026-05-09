b892f6b53ec717db2639ab0c43235f6fe3d7fe48
전재명 <jeonjm0130@naver.com>
2026-05-09 21:19:21 +0900

    js std 4

--- javaScript/basics-01-starting-project/assets/scripts/app.js
+++ javaScript/basics-01-starting-project/assets/scripts/app.js
@@ -1,61 +1,73 @@
 const defaultResult = 0;
 
 let currentResult = defaultResult;
+let logEntries = [];
+const a = { name: 'KIM' };
 
-function getUserInput(){
-    return parseInt(userInput.value);
+function getUserInput() {
+  return parseInt(userInput.value);
 }
 
-
-function createAndWriteOutput(연산자, 결과전값, 투입값){
-    const calcDescription = `${결과전값} ${연산자} ${투입값}`;
-    outputResult(currentResult, calcDescription);
+function createAndWriteOutput(연산자, 결과전값, 투입값) {
+  const calcDescription = `${결과전값} ${연산자} ${투입값}`;
+  outputResult(currentResult, calcDescription);
 }
 
-function add(){
-    const enterNumber = getUserInput();
-    const initialResult = currentResult;
-    currentResult = currentResult + enterNumber;
-    createAndWriteOutput('+', initialResult, enterNumber);
-    
-     
+function writeToLog(연산자, initialResult, enterNumber, currentResult) {
+  const logEntry = {
+    operation: 연산자,
+    preResult: initialResult,
+    number: enterNumber,
+    result: currentResult,
+  };
+  logEntries.push(logEntry);
 }
 
-function subtract(){
-    const enterNumber = getUserInput();
-    const initialResult = currentResult;
-    currentResult = currentResult - enterNumber;
-    createAndWriteOutput('-', initialResult, enterNumber);
-     
+function add() {
+  const enterNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult + enterNumber;
+  createAndWriteOutput('+', initialResult, enterNumber);
+  writeToLog('ADD', initialResult, enterNumber, currentResult);
+  console.log(logEntries);
 }
 
-function multiply(){
-    const enterNumber = getUserInput();
-    const initialResult = currentResult;
-    currentResult = currentResult * enterNumber;
-    createAndWriteOutput('*', initialResult, enterNumber);
-     
+function subtract() {
+  const enterNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - enterNumber;
+  createAndWriteOutput('-', initialResult, enterNumber);
+  writeToLog('SUBRACT', initialResult, enterNumber, currentResult);
+  console.log(logEntries);
 }
 
-function divide(){
-    const enterNumber = getUserInput();
-    const initialResult = currentResult;
-    currentResult = currentResult / enterNumber;
-    createAndWriteOutput('/', initialResult, enterNumber);
-     
+function multiply() {
+  const enterNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * enterNumber;
+  createAndWriteOutput('*', initialResult, enterNumber);
+  writeToLog('MULTIPLY', initialResult, enterNumber, currentResult);
+  console.log(logEntries);
 }
 
-function initalizeResult(){
-    currentResult = 0;
-    outputResult(currentResult, "back to Zero");
+function divide() {
+  const enterNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / enterNumber;
+  createAndWriteOutput('/', initialResult, enterNumber);
+  writeToLog('DIVIDE', initialResult, enterNumber, currentResult);
+  console.log(logEntries);
+}
+
+function initalize() {
+  currentResult = 0;
+  outputResult(currentResult, 'Initalized!');
+  logEntries = [];
+  console.log(logEntries);
 }
 
 addBtn.addEventListener('click', add);
-
 subtractBtn.addEventListener('click', subtract);
-
 multiplyBtn.addEventListener('click', multiply);
-
 divideBtn.addEventListener('click', divide);
-
-initBtn.addEventListener('click', initalizeResult);
+initBtn.addEventListener('click', initalize);
