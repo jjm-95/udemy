c012590a2b3fe8e326fa6e8a01c1f854406b2995
전재명 <jeonjm0130@naver.com>
2026-05-21 23:14:12 +0900

    js std 8

--- javaScript/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ javaScript/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,15 +1,21 @@
 const ATTACK_VALUE = 10;
 const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
-const HEAL_VALUE = 20;
 
-let chosenMaxLife = 100;
+const eneterdValue = prompt('최대 생명을 고르시오', '100');
+let chosenMaxLife = parseInt(eneterdValue);
+
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+  chosenMaxLife = 100;
+}
 
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
 
+const HEAL_VALUE = chosenMaxLife / 5;
 let currentHealAmount = 2;
 healAmount.value = currentHealAmount;
 healAmount.innerText = healAmount.value;
@@ -18,17 +24,45 @@ let currentStrongAttackAmount = 3;
 strongAttackAmount.value = currentStrongAttackAmount;
 strongAttackAmount.innerText = strongAttackAmount.value;
 
+function reset() {
+  currentMonsterHealth = chosenMaxLife;
+  currentPlayerHealth = chosenMaxLife;
+  hasBonusLife = true;
+  currentHealAmount = 2;
+  healAmount.value = currentHealAmount;
+  healAmount.innerText = healAmount.value;
+  currentStrongAttackAmount = 3;
+  strongAttackAmount.value = currentStrongAttackAmount;
+  strongAttackAmount.innerText = strongAttackAmount.value;
+  document.getElementById('monster').style.textDecoration = 'none';
+  document.getElementById('player').style.textDecoration = 'none';
+  resetGame(chosenMaxLife);
+}
+
 function endRound() {
+  const initialPlayerHealth = currentPlayerHealth;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth = currentPlayerHealth - playerDamage;
+
+  // 체력이 0이 될 경우 보너스생명을 차감해 데미지를 무효화
+  if (currentPlayerHealth <= 0 && hasBonusLife === true) {
+    hasBonusLife = false;
+    removeBonusLife();
+    currentPlayerHealth = initialPlayerHealth;
+    setPlayerHealth(initialPlayerHealth);
+    alert('얼방 생성!');
+  }
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
-    alert('너 이김!');
     document.getElementById('monster').style.textDecoration = 'line-through';
+    alert('너 이김!');
+    reset();
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
-    alert('너 짐');
     document.getElementById('player').style.textDecoration = 'line-through';
+    alert('너 짐');
+    reset();
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
     alert('무승부');
+    reset();
   }
 }
 
@@ -43,6 +77,7 @@ function attackMonster(mode) {
 
   if (currentMonsterHealth <= 0 || currentPlayerHealth <= 0) {
     alert('게임 끝!');
+    reset();
   } else {
     const damage = dealMonsterDamage(maxDamage);
     currentMonsterHealth = currentMonsterHealth - damage;
@@ -57,6 +92,7 @@ function attackHandler() {
 function strongAttackHandler() {
   if (currentMonsterHealth <= 0 || currentPlayerHealth <= 0) {
     alert('게임 끝!');
+    reset();
   } else if (currentStrongAttackAmount <= 0) {
     alert('힘 딸림;;');
   } else {
@@ -71,6 +107,7 @@ function healPlayerHandler() {
   if (currentMonsterHealth <= 0 || currentPlayerHealth <= 0) {
     // 체력이 한쪽이 0이 된 경우 게임 진행 방지
     alert('게임 끝!');
+    reset();
   } else if (currentHealAmount <= 0) {
     // 힐 갯수가 0이 된 경우 힐 방지
     alert('회복 할 수 없슴!');
